10dc3b632d58bf4b7dfe1222cdb0983a4f910276
tonyfast <tony.fast@gmail.com>
2018-03-21 21:31:51 -0400

    Add the top level notebooks

---
+++ setup.py
@@ -0,0 +1,12 @@
+__import__('setuptools').setup(
+    name="ten",
+    version="0.0.1",
+    author="deathbeds", author_email="tony.fast@gmail.com",
+    description="Tools for writing, righting, and riting notebooks.", 
+    license="BSD-3-Clause",
+    setup_requires=[],
+    tests_require=[],
+    install_requires=[],
+    include_package_data=True,
+    packages=['ten'],
+)
---
+++ ten/__init__.py
@@ -0,0 +1,1 @@
+logo = "https://user-images.githubusercontent.com/4236275/35933146-bfb7ec94-0c07-11e8-98d5-8972dc4b4e39.png"
